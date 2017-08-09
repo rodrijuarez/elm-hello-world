d976dce5beab3dc825c0cd29c699785d09bef3fd
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 11:05:56 -0300

    Cleaning code

--- Main.elm
+++ Main.elm
@@ -1,20 +1,5 @@
 module Main exposing (..)
 import Html exposing (text)
 
-politely: String -> String
-
-politely phrase = 
-    "Excuse me, " ++ phrase
-
-ask: String -> String -> String
-
-ask thing place =
-    "is there a " ++ thing ++ " in the " ++place ++ "?"
-
-askPolitelyAboutFish: String -> String
-
-askPolitelyAboutFish =
-    politely << ask "fish"
-
 main = 
-    text <| askPolitelyAboutFish "kitchen"
+    text <| "Hello, your majesty"
