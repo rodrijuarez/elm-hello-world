ebbbd6d40f1e7b001fe2a60f7027ce361da7c183
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 11:10:08 -0300

    Created list of records

--- Main.elm
+++ Main.elm
@@ -1,8 +1,23 @@
 module Main exposing (..)
 import Html exposing (text)
 
-foo =
-    ["one", "two", "three"]
+type alias Person = 
+    { name: String
+    , age: Int
+    }
+
+
+people =
+    [{
+        name = "Legolas",
+        age = 31
+    }, {
+        name = "Aragon",
+        age = 40
+    }, {
+        name = "Gandalf",
+        age = 60
+    }]
 
 main = 
-    text <| toString <| foo
+    text <| toString <| people
