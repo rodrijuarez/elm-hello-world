916e5ffb9d438b81021f21b3bd9314d26d813861
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 11:22:04 -0300

    Created function that finds a Record in the List by the name attribute

--- Main.elm
+++ Main.elm
@@ -24,5 +24,16 @@ names: List Person -> List String
 
 names persons = List.map (\person -> person.name) persons
 
+findPerson: String -> List Person -> Maybe Person
+
+findPerson name persons =
+    List.foldl
+    (\person memo -> case memo of
+        Just _ -> memo
+        Nothing -> if person.name  == name then
+         Just person
+         else Nothing
+    ) Nothing persons
+
 main = 
-    text <| toString <| names people
+    text <| toString <| findPerson "Legolas" people
