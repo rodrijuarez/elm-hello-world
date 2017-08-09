0f307b0ca05738e5b289d9d12dbaac75cb652634
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 10:58:00 -0300

    Render function that takes a Record and returns a String

--- Main.elm
+++ Main.elm
@@ -1,10 +1,20 @@
 module Main exposing (..)
 import Html exposing (text)
 
+type alias Dog = 
+    { name: String
+    , age: Int
+    }
+
 dog =
     { name = "Spock"
     , age = 3
     }
 
+renderDog: Dog -> String
+
+renderDog dog =
+     dog.name ++ ", " ++ toString dog.age
+
 main = 
-    text <| dog.name
+    text <| renderDog dog
