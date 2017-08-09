dcf53a8f220ba416d54d17a54a00c15516342d3e
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 11:28:49 -0300

    Basic code for this exercise

--- Main.elm
+++ Main.elm
@@ -1,5 +1,19 @@
 module Main exposing (..)
 import Html exposing (text)
 
+type alias Ship =
+    { name: String
+    , model: String
+    , cost: Int
+    }
+
+ships =
+    [ { name = "X-Wing", cost = 100}
+    , { name = "Millenium Falcon", cost = 200 }
+    , { name = "Death Star", cost = 300}
+    ]
+
 main = 
-    text <| "Hello, your majesty!"
+    text 
+    <| toString 
+    <| ships
