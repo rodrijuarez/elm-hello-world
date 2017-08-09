4a5db391a5b53c42938709210f9a375eb00a05f9
Rodrigo Juarez <rodrigo.juarez.inf@gmail.com>
2017-08-09 10:52:30 -0300

    Record creation in Elm

--- Main.elm
+++ Main.elm
@@ -1,20 +1,10 @@
 module Main exposing (..)
 import Html exposing (text)
 
-politely: String -> String
-
-politely phrase = 
-    "Excuse me, " ++ phrase
-
-ask: String -> String -> String
-
-ask thing place =
-    "is there a " ++ thing ++ " in the " ++place ++ "?"
-
-askPolitelyAboutFish: String -> String
-
-askPolitelyAboutFish =
-    politely << ask "fish"
+dog =
+    { name = "Spock"
+    , age = 3
+    }
 
 main = 
-    text <| askPolitelyAboutFish "kitchen"
+    text <| dog.name
